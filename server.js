551be107575088ae3025b416fed58609983f49d6
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-09-19 21:45:07 -0700

    read file and write data

--- server.js
+++ server.js
@@ -13,10 +13,16 @@ http.createServer((request, response) => {
         filePath = 'index.html';
     }
         
+    fs.readFile(filePath, (err, data) =>{
+        if (err) {
+            throw err;
+        }
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
 
-    })
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Node!\n');
+    });
+    
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
