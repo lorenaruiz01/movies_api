a43a6d56c2e4a6a5dcc1e18d6f719922b5a04bf1
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-09-19 18:05:05 -0700

    end response and send back message

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const http = require('http');
 
 http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-})
+    response.end('Hello Node!\n');
+}).listen(8080);
 
