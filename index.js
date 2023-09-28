479b782be1bedcf4da80fd3497781e0a6149a0cc
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2023-09-27 18:25:40 -0700

    declare allMovies variable with sample objects

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const app = express();
+
+let allMovies = [
+    {
+        title: 'movie 1',
+        director: 'Some Director'
+    },
+    {
+        title: 'movie 2',
+        director: 'Another Director'
+    },
+    {
+        title: 'movie 3',
+        director: 'Yet Another Director'
+    }
+];
