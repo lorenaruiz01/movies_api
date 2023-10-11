d4c0b7d919b1831d235cf97d7d540f80bd08f31a
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2023-10-10 19:23:03 -0700

    get movies by genre

--- server.js
+++ server.js
@@ -102,16 +102,19 @@ app.get('/movies/:title', (req, res) => {
 });
 
 // returns movies by genre
-app.get('/movies/genre/:genreName', (req, res) => {
+app.get('/movies/:genre', (req, res) => {
     const { genreName } = reg.params;
-    const moviesByGenre = movies.find( movies => movies.genre === genreName);
-})
+    const moviesByGenre = movies.find( movies => movies.genre.genreName === genreName);
 
     if (moviesByGenre) {
-        res.status(200).json(moviesByGenre)
+        res.status(200).json(moviesByGenre);
     } else {
         res.status(400).send('no movies in that genre')
     }
+})
+
+
+
 
 // listening on port 8080
 app.listen(8080, () => {
